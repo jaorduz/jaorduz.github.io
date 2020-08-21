634db00e5aa537fd5f7ad93eb637a50dcdd62e42
Javier Orduz <jaorduz@ciencias.unam.mx>
2020-08-21 00:17:46 -0500

    100002-20082020

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,17 +1,38 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Long Beach, USA (ICML)",
+    33.763889,
+    -118.18833
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Montreal, CAN (NeuRIPS)",
+    45.502664,
+    -73.55599
   ],
   [
     "Naucalpan de Juarez, MX",
     19.7167,
     -96.8333
+  ],
+    [
+    "Sanya, CHINE (Tsinghua Sanya International Mathematics Forum)",
+    "http://www.tsimf.cn/en/index"
+    18.3192,
+    -109.4156
+  ],
+  [
+    "Siegen, GER (Siegen University)",
+    50.9117,
+    -8.0254
+  ],
+    [
+    "Mainz, GER (Mainz University, Johannes Gutenberg)",
+    49.9939,
+    -8.2415
+  ],
+    [
+    "Sao Paulo, BRA (ICTP)",
+    -23.5242,
+    -46.6646
   ]
 ];
