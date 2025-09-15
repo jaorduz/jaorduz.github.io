33d65a49f5b81c796ff8a4feba92589c9d47008c
Javier Orduz <jaorduz@ieee.org>
2025-09-14 21:18:04 -0500

    V1Sep14

---
+++ _site/talkmap/org-locations.js
@@ -0,0 +1,75 @@
+
+var addressPoints = [
+["Paris, France (France Quantum Conference)",
+  48.83346054300767, 
+  2.3718322417021596
+],
+  ["Padova, Italy (University of Padova)",
+    45.40891807742582, 
+    11.882669908502704
+  ],
+  ["Anaheim, USA (APS Global Physics Summit)",
+    33.80150050031274, 
+    -117.92085529486332
+  ],
+  [
+    "Oxford, USA (Miami University)",
+    39.511245980436, 
+    -84.73139637245366
+  ],
+  [
+    "Las Vegas, USA (CSCI)",
+    36.12633074771421, 
+    -115.18468268517731
+  ],
+  [
+    "Waco, USA (BU)",
+    31.54867,
+    -97.11322
+  ],
+  [
+    "Long Beach, USA (ICML)",
+    33.763889,
+    -118.18833
+  ],
+  [
+    "Montreal, CAN (NeuRIPS)",
+    45.504,
+    -73.561
+  ],
+  [
+    "Naucalpan de Juarez, MX (FESAc-UNAM)",
+    19.4818,
+    -99.2454
+  ],
+    [
+    "Sanya, CHINA (Tsinghua Sanya International Mathematics Forum)",
+    18.3192,
+    109.4156
+  ],
+  [
+    "Siegen, GER (Siegen University)",
+    50.9117,
+    8.0254
+  ],
+    [
+    "Mainz, GER (Mainz University, Johannes Gutenberg)",
+    49.9939,
+    8.2415
+  ],
+    [
+    "Sao Paulo, BRA (ICTP)",
+    -23.5242,
+    -46.6646
+  ],
+    [
+    "Antigua, GUA (SILAFAE)",
+    14.5595,
+    -90.7294,
+  ],
+    [
+    "Oxford, ENG (ML Shool)",
+    51.7619,
+    -1.2623,
+  ]
+];
